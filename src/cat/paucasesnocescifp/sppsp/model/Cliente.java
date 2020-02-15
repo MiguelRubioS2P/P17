5dbebdc1bb77cd94cd34094662e6d0d14ce33fc3
Miguel Rubio <miguelrubio@paucasesnovescifp.cat>
2020-02-15 16:52:15 +0100

    no funciona

--- src/cat/paucasesnocescifp/sppsp/model/Cliente.java
+++ src/cat/paucasesnocescifp/sppsp/model/Cliente.java
@@ -1,4 +1,76 @@
 package cat.paucasesnocescifp.sppsp.model;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Cliente {
+
+    public static void main(String[] args) {
+
+        //Okey,bye
+        boolean abierto = true;
+        try {
+            // Inicializamos socket cliente con un puerto y una ip predeterminada
+
+            Socket socket = new Socket("127.0.0.1", 5555);
+            socket.setKeepAlive(true);
+
+            System.out.println("Connexió iniciada");
+
+            /*// Se inicializa la entrada de informacion del cliente - el msg del servidor al cliente
+            DataInputStream entrada = new DataInputStream(socket.getInputStream());
+            // se inicializa la salida de informacion del cliente - el msg del cliente al servidor
+            DataOutputStream sortida = new DataOutputStream(socket.getOutputStream());
+
+            System.out.println("Introdueixi el missatge que voleu enviar:");
+            // Escaner el cual permite personalizar el texto enviado al servidor
+            Scanner sc = new Scanner(System.in);
+
+            // se envia lo que se escribe al servidor
+            sortida.writeUTF(sc.nextLine());
+            System.out.println("El servidor dice: " +entrada.readUTF());
+
+            // Cierre de objetos socket, InputStream y OutputStream.
+            entrada.close();
+            sortida.close();
+            socket.close();*/
+
+            while (abierto) {
+
+
+
+                // Se inicializa la entrada de informacion del cliente - el msg del servidor al cliente
+                DataInputStream entrada = new DataInputStream(socket.getInputStream());
+                // se inicializa la salida de informacion del cliente - el msg del cliente al servidor
+                DataOutputStream sortida = new DataOutputStream(socket.getOutputStream());
+
+                System.out.println("Introdueixi el missatge que voleu enviar:");
+                // Escaner el cual permite personalizar el texto enviado al servidor
+                Scanner sc = new Scanner(System.in);
+
+                String dato = sc.nextLine();
+                if("Tengo que programar".equals(dato)){
+                    System.out.println("Cerrando la conexión con el servidor");
+                    abierto = false;
+                    sortida.writeUTF(dato);
+                }else{
+                    sortida.writeUTF(dato);
+                    System.out.println("El servidor dice: " + entrada.readUTF());
+                }
+                // se envia lo que se escribe al servidor
+
+
+                // Cierre de objetos socket, InputStream y OutputStream.
+
+
+            }
+            socket.close();
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
--- src/cat/paucasesnocescifp/sppsp/model/Servidor.java
+++ src/cat/paucasesnocescifp/sppsp/model/Servidor.java
@@ -7,23 +7,66 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+//El contingut del missatge dependrà de la frase rebuda:
+//
+//Si la frase és "Com et dius?", respondrà amb la cadena "Em dic Toni Ballador".
+//
+//Si la frase és "Quantes línies de codi tens?", respondrà amb el nombre de línies del teu codi de servidor.
+//
+//Si la frase es qualsevol altra cosa, respondrà "No he entès la pregunta".
+//
+//Aquests missatges s'han de mostrar pel client!
+
 public class Servidor {
 
     public static void main(String[] args) {
 
+        Boolean abierto = true;
         //Iniciamos el socket del servidor con un puerto por defecto
         try {
             ServerSocket serverSocket = new ServerSocket(5555);
 
-            //iniciamos un socket que seran los clientes que se conecten a nuestro servidor
-            Socket socketCliente = serverSocket.accept();
+            System.out.println("Servidor iniciado");
 
-            //Inicializamos la entrada de la información que el cliente manda al servidor
-            DataInputStream entrada = new DataInputStream(socketCliente.getInputStream());
+            while(abierto){
+                //iniciamos un socket que seran los clientes que se conecten a nuestro servidor
+                Socket socketCliente = serverSocket.accept();
 
-            //Inicializamos la respuesta del servidor mandara al cliente
-            DataOutputStream salida = new DataOutputStream(socketCliente.getOutputStream());
+                //Inicializamos la entrada de la información que el cliente manda al servidor
+                DataInputStream entrada = new DataInputStream(socketCliente.getInputStream());
 
+                //Inicializamos la respuesta del servidor mandara al cliente
+                DataOutputStream salida = new DataOutputStream(socketCliente.getOutputStream());
+
+                switch (entrada.readUTF()){
+                    case "Com et dius?":
+                        salida.writeUTF("Em dic Toni Ballador");
+                        break;
+                    case "Quantes línies de codi tens?":
+                        salida.writeUTF("Tengo 1 linea porque soy buenisimo");
+                        break;
+                    case "Tengo que programar":
+                        salida.writeUTF("Okey,bye");
+                        System.out.println("Servidor apagado");
+                        abierto = false;
+                        //serverSocket.close();
+                        break;
+                    default:
+                        salida.writeUTF("No he entès la pregunta");
+                        break;
+                }
+
+                entrada.close();
+                salida.close();
+                socketCliente.close();
+
+            }
+
+            serverSocket.close();
+
+
+            //System.out.println("Servidor apagado");
+            //serverSocket.close();
 
         } catch (IOException e) {
             System.out.println("Error Servidor: " + e.getMessage());
